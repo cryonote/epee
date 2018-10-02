f112534f18f4a74e9e5cbacd6a3077bb87680d86
anythingtechpro <anythingtechpro@gmail.com>
2018-10-01 22:20:42 -0400

    include/storages: fixed an issue with moving local objects in a return statement

--- include/storages/portable_storage_template_helper.h
+++ include/storages/portable_storage_template_helper.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2006-2013, Andrey N. Sabelnikov, www.sabelnikov.net
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // * Redistributions of source code must retain the above copyright
@@ -11,7 +11,7 @@
 // * Neither the name of the Andrey N. Sabelnikov nor the
 // names of its contributors may be used to endorse or promote products
 // derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,7 +22,7 @@
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 
 #pragma once
 
@@ -72,7 +72,7 @@ namespace epee
     {
       std::string json_buff;
       store_t_to_json(str_in, json_buff, indent, insert_newlines);
-      return std::move(json_buff);
+      return json_buff;
     }
     //-----------------------------------------------------------------------------------------------------------
     template<class t_struct>
@@ -117,7 +117,7 @@ namespace epee
     {
       std::string binary_buff;
       store_t_to_binary(str_in, binary_buff, indent);
-      return std::move(binary_buff);
+      return binary_buff;
     }
   }
 }
